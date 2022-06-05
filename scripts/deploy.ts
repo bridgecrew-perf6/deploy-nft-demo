1794b3f642fccfaacdfa2efbe1b2e900e7c84994
masamichhhhi <masamichhhi@gmail.com>
2022-06-05 21:57:48 +0900

    First commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,15 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+  const myNFT = await MyNFT.deploy();
+  await myNFT.deployed();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
